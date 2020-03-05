eaafbc1a5f00b19d7e0d48ad0e9762266c2af3e7
Dmitry <dima@iMac-Dmitrij.local>
2020-03-05 10:30:47 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Дмитрий Алдошкин on 05.03.2020.
+//  Copyright © 2020 Дмитрий Алдошкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
